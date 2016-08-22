71ff49759ba02f82eba7bbfc0f1571fc4347a6fb
huifeidepangzi <huifeidepangzi@gmail.com>
2016-08-22 17:14:18 +1200

    The frame code of the assign1

---
+++ 251-assignment1.py
@@ -0,0 +1,47 @@
+import tkinter as tk
+
+window = tk.Tk()
+window.title('myEditor');
+
+window.geometry('1000x600')
+
+l = tk.Label(window, bg='yellow',width=20,text='')
+l.pack()
+counter=0
+
+def do_job():
+    global counter
+    l.config(text='do '+str(counter))
+    counter+=1
+
+menubar = tk.Menu(window)
+
+filemenu = tk.Menu(menubar,tearoff=0)
+menubar.add_cascade(label='File',menu=filemenu)
+
+filemenu.add_command(label='New',command=do_job)
+filemenu.add_command(label='Open',command=do_job)
+filemenu.add_command(label='Save',command=do_job)
+filemenu.add_separator()
+filemenu.add_command(label='Exit',command=window.quit)
+
+searchmenu = tk.Menu(menubar,tearoff=0)
+menubar.add_cascade(label='Search',menu=searchmenu)
+
+searchmenu.add_command(label='Cut',command=do_job)
+searchmenu.add_command(label='Copy',command=do_job)
+searchmenu.add_command(label='Paste',command=do_job)
+
+viewmenu = tk.Menu(menubar,tearoff=0)
+menubar.add_cascade(label='View',menu=viewmenu)
+
+helpmenu = tk.Menu(menubar,tearoff=0)
+menubar.add_cascade(label='Help',menu=helpmenu)
+
+t = tk.Text(window,height=100, width=100)
+t.pack()
+
+
+window.config(menu=menubar)
+
+window.mainloop()
