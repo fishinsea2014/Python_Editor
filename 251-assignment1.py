bb2eea10720c00a65868199b4fc0ff64140a99d7
JASON QU <fishinsea2014@gmail.com>
2016-08-23 15:42:45 +1200

    Add functions of cut, copy  and paste.

--- 251-assignment1.py
+++ 251-assignment1.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+# import ScrolledText
 
 window = tk.Tk()
 window.title('myEditor');
@@ -7,12 +8,19 @@ window.geometry('1000x600')
 
 l = tk.Label(window, bg='yellow',width=20,text='')
 l.pack()
+
+t = tk.Text(window,height=100, width=100)
+t.pack()
+
 counter=0
 
 def do_job():
     global counter
     l.config(text='do '+str(counter))
     counter+=1
+
+# def do_j_insert():
+#     t.insert('current', "hello,    word" )
 
 menubar = tk.Menu(window)
 
@@ -28,18 +36,31 @@ filemenu.add_command(label='Exit',command=window.quit)
 searchmenu = tk.Menu(menubar,tearoff=0)
 menubar.add_cascade(label='Search',menu=searchmenu)
 
-searchmenu.add_command(label='Cut',command=do_job)
-searchmenu.add_command(label='Copy',command=do_job)
-searchmenu.add_command(label='Paste',command=do_job)
-
+# searchmenu.add_command(label='Cut',command=do_job)
+searchmenu.add_command(label="Cut",\
+                       accelerator="Ctrl+x",\
+                       command = lambda: \
+                                 t.event_generate('<<Cut>>'))
+# searchmenu.add_command(label='Copy',command=do_job)
+searchmenu.add_command(label="Copy",\
+                       accelerator="Ctrl+c",\
+                       command = lambda: \
+                                 t.event_generate('<<Copy>>'))
+# searchmenu.add_command(label='Paste',command=do_j_insert)
+searchmenu.add_command(label="Paste",\
+                       accelerator="Ctrl+v",\
+                       command = lambda: \
+                                 t.event_generate('<<Paste>>'))
 viewmenu = tk.Menu(menubar,tearoff=0)
 menubar.add_cascade(label='View',menu=viewmenu)
 
 helpmenu = tk.Menu(menubar,tearoff=0)
 menubar.add_cascade(label='Help',menu=helpmenu)
 
-t = tk.Text(window,height=100, width=100)
-t.pack()
+
+
+# searchmenu.add_command(label='Paste',command=do_j_insert())
+
 # t.selection_get()
 
 
